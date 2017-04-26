45e8dffea3052c61d167ee52fe0c32219260422b
ximet <dq.ximet@gmail.com>
2017-04-26 19:41:34 +0300

    update vdom

--- src/vdom/vdom.js
+++ src/vdom/vdom.js
@@ -4,17 +4,31 @@ export function createVirtualNode(type, props, ...children) {
 }
 
 export default class VirtualNode {
-    constructor(tag, props, data, children, text, options) {
+    constructor(tag, props, children, text, isCloned) {
         this.tag = tag;
         this.props = props;
-        this.data = data;
         this.children = children;
         this.text = text;
-        this.options = options;
+        this.isCloned = isCloned;
     }
 }
 
 
-export function createTextVNode (value) {
-    return new VirtualNode(undefined, undefined, undefined, undefined, String(value))
+export function createTextVirtualNode (value) {
+    return new VirtualNode(undefined, undefined, undefined, String(value))
+}
+
+export function clonedVirtualNode(node) {
+    return new VirtualNode(node.tag, node.props, node.children, node.text, true)
+}
+
+export function clonedVirtualNodes(nodes) {
+    const length = nodes.length;
+    const resultCloneArray = new Array(length);
+
+    for (let i = 0; i < length; i++) {
+        resultCloneArray[i] = clonedVirtualNode(nodes[i])
+    }
+
+    return resultCloneArray;
 }
