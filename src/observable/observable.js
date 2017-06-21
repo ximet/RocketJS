86dc5fa5ebdf8a21dd571e4ba834557abbc6a9af
ximet <dq.ximet@gmail.com>
2017-06-21 20:06:03 +0300

    add observers

--- src/observable/observable.js
+++ src/observable/observable.js
@@ -1,13 +1,19 @@
-export default class Observable {
-  constructor (instance) {
-    this.observers = []
-  }
-}
-
 export const hasObservers = (observable) => {
   return observable.observers && observable.observers.length > 0
 }
 
 export const getObservers = (observable) => {
   return observable.observers
+}
+
+export const addObservers = (observable, node) => {
+  const length = observable.observers.length;
+
+  observable.observers[length] = node;
+}
+
+export default class Observable {
+  constructor (instance) {
+    this.observers = []
+  }
 }
