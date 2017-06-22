2f0ef98cceee89b2ad2e5af0949f457bc7604bf8
ximet <dq.ximet@gmail.com>
2017-06-22 18:09:53 +0300

    add observableTest mock

---
+++ test/observableTest.js
@@ -0,0 +1,13 @@
+import { getObservers, hasObservers, addObservers, Observable } from '../src/vdom/vdom.js';
+import expect from 'expect';
+
+describe('Test Observable', () => {
+
+    describe('Check getObservers', () => {
+        it('test function check getObservers', () => {
+
+        });
+    });
+
+
+});
