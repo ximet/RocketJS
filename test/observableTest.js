d1958e84aa1963f87331f23f2cd127857fee9369
ximet <dq.ximet@gmail.com>
2017-06-22 18:22:11 +0300

    add test for hasObservers

--- test/observableTest.js
+++ test/observableTest.js
@@ -2,13 +2,15 @@ import { getObservers, hasObservers, addObservers, Observable } from '../src/obs
 import expect from 'expect';
 
 describe('Test Observable', () => {
-
+    const correctObservable = {
+        observers: []
+    };
+    const fullObservable = {
+        observers: [1, 2, 3]
+    };
+    const uncorrectObservable = {};
     describe('Check getObservers', () => {
         it('test function check getObservers', () => {
-            const correctObservable = {
-                observers: []
-            };
-            const uncorrectObservable = {};
 
             expect(getObservers(correctObservable)).toEqual(correctObservable.observers);
             expect(getObservers(uncorrectObservable)).toEqual(undefined);
@@ -16,5 +18,12 @@ describe('Test Observable', () => {
         });
     });
 
+    describe('Check hasObservers', () => {
+        it('test function check hasObservers', () => {
 
+            expect(hasObservers(fullObservable)).toEqual(true);
+            expect(hasObservers(correctObservable)).toEqual(false);
+            expect(hasObservers(uncorrectObservable)).toEqual(undefined);
+        });
+    });
 });
