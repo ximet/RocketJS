8c9b43f064ec3949f4fc2ecdd46074036125571b
ximet <dq.ximet@gmail.com>
2017-06-22 18:16:47 +0300

    add first test for observers

--- test/observableTest.js
+++ test/observableTest.js
@@ -1,10 +1,17 @@
-import { getObservers, hasObservers, addObservers, Observable } from '../src/vdom/vdom.js';
+import { getObservers, hasObservers, addObservers, Observable } from '../src/observable/observable.js';
 import expect from 'expect';
 
 describe('Test Observable', () => {
 
     describe('Check getObservers', () => {
         it('test function check getObservers', () => {
+            const correctObservable = {
+                observers: []
+            };
+            const uncorrectObservable = {};
+
+            expect(getObservers(correctObservable)).toEqual(correctObservable.observers);
+            expect(getObservers(uncorrectObservable)).toEqual(undefined);
 
         });
     });
